205417940b54b01fab0f19d26268c467008e13a7
gemuelg <gemuelgrande@gmail.com>
2025-08-10 04:58:41 +0800

    csp change 2

--- next.config.mjs
+++ next.config.mjs
@@ -10,29 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-eval' 'unsafe-inline';
-              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-              img-src 'self' https://image.tmdb.org blob: data:;
-              font-src 'self' https://fonts.gstatic.com data:;
-              connect-src 'self' https://api.themoviedb.org;
-              frame-src 'self' https://hnembed.cc https://multiembed.mov; // UPDATED
-              media-src 'self' https://hnembed.cc https://multiembed.mov; // UPDATED
-            `.replace(/\n/g, ''),
-          },
-        ],
-      },
-    ];
-  },
 };
- 
+
 export default nextConfig;
