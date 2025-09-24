c6d36dd22331832e405c90cc38b9da29ae120ef9
gemuelg <gemuelgrande@gmail.com>
2025-09-25 06:02:10 +0800

    Add site verification metadata for improved SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+// app/layout.tsx
+
 import './globals.css';
 import { ReactNode } from 'react';
 import Navbar from '@/components/Navbar';
@@ -7,6 +9,11 @@ import { Analytics } from '@vercel/analytics/next';
 export const metadata = {
   title: 'StreamWave',
   description: 'Your next-generation streaming platform',
+  
+  
+  other: {
+    '6a97888e-site-verification': '7154d61544ed1ed744499f4e817d53dd',
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
